2086fb4a2c871fb44464c35e3c993670ed2bbafc
Alena Eske <alenaeske86@gmail.com>
2024-05-28 14:25:54 +0300

    Написан метод get_vehicle_advice для советов по приобретению ТС

--- class_vehicle.py
+++ class_vehicle.py
@@ -1,12 +1,24 @@
 class Vehicle:
-    def __init__(self, name, mileage):
+    def __init__(self, name):
         self.name = name
-        self.mileage = mileage
+
 
     def get_vehicle_type(self, wheel):
         if wheel == 2:
           return f"Это мотоцикл марки {self.name}!"
         elif wheel == 3:
             return f"Это трицикл марки {self.name}"
-        else:
+        elif wheel == 4:
             return f"Это автомобиль марки {self.name}"
+        else:
+            return f"Я таких не знаю."
+
+    def get_vehicle_advice(self, mileage):
+        self.mileage = mileage
+        if 50001 >= mileage <= 100000:
+            return f"Неплохо! {self.name} можно купить!"
+        elif 100001 >= mileage <= 150000:
+            return f"{self.name} надо внимательно проверить!"
+        elif mileage >= 150001:
+            return f"{self.name} лучше не брать!"
+

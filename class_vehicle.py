718ff630997e4d6b26a8c930e2637297d482c3a7
Alena Eske <alenaeske86@gmail.com>
2024-05-28 14:09:12 +0300

    Написан метод get_vehicle_type для определения вида ТС

--- class_vehicle.py
+++ class_vehicle.py
@@ -3,3 +3,10 @@ class Vehicle:
         self.name = name
         self.mileage = mileage
 
+    def get_vehicle_type(self, wheel):
+        if wheel == 2:
+          return f"Это мотоцикл марки {self.name}!"
+        elif wheel == 3:
+            return f"Это трицикл марки {self.name}"
+        else:
+            return f"Это автомобиль марки {self.name}"

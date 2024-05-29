f536eae61f69ee80795b2754f6440b69f25676e4
Alena Eske <alenaeske86@gmail.com>
2024-05-29 08:21:38 +0300

    Добавлен метод get_hotel_of_stars для выбора отеля

--- class_Vacation.py
+++ class_Vacation.py
@@ -1,3 +1,16 @@
 class Hotel:
     def __init__(self, name):
         self.name = name
+    def get_hotel_of_stars(self, star):
+
+        if star <= 2:
+            included = ["трансфер"]
+            return f" {self.name} - плохой отель. Там только {included}"
+        elif star == 3:
+            included = ["трансфер", "завтрак", "wi-fi"]
+            return f"{self.name} - нормальный отель. Включено {included}. Но далеко от моря! "
+        elif star == 4:
+            included = ["трансфер", "шведский стол", "анимация", "wi-fi"]
+            return f"{self.name} - отличный отель, включено {included}."
+        else:
+            return f"{self.name} - супер отель, но очень дорогой."
